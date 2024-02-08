8fd6bae1df9ceb967c7769a0ae8544e2dc859729
Caleb Andreano <calebandreano@gmail.com>
2024-02-08 12:58:36 -0600

    notebook

--- point-in-polygon.py
+++ point-in-polygon.py
@@ -10,10 +10,12 @@ def inside(points, target):
     avg_point = (x_sum/len(points), y_sum/len(points))
     avg_slope = (avg_point[1] - target[1])/(avg_point[0] - target[0])
 
-    for i in range(0, len(points) - 1):
+    edges = []
 
-        allisondr
-        mattsona
+
+    for i in range(0, len(points) - 1):
+        
+
 
         
 
