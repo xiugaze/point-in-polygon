128af09f099af424b6392e78c9d9f6423a6e18f5
Allison <allisondr@msoe.edu>
2024-02-10 23:30:48 -0600

    Report template completed.

--- point-in-polygon.py
+++ point-in-polygon.py
@@ -1,11 +1,20 @@
+import math
+
+
 # The intersection function returns the point of intersection between the
 # line that is made from p1 and p0 and the horizontal line y.
 def intersection(p1, p0, y):
-    return (y * (p1[0] - p0[0]) - p0[1] * (p1[0] - p0[0]) - p0[0] * (p1[1] - p0[1])) / (p1[1] - p0[1])
+    return math.nan \
+        if p1[1] - p0[1] == 0 \
+        else -(y * (p1[0] - p0[0]) - p0[1] * (p1[0] - p0[0]) - p0[0] * (p1[1] - p0[1])) / (p1[1] - p0[1])
+
+
+def in_bounds(min_x, intersect_x, max_x):
+    return not math.isnan(intersect_x) and min_x <= intersect_x <= max_x
 
 
 # The inside function determines if a point is inside a polygon made of other points.
-def inside(points, target):
+def in_polygon(points, target):
     count = 1 if points[0][1] == target[1] else 0
     max_x = points[0][0]
     # Finds the bounds that the intersections must be made in.
@@ -22,16 +31,21 @@ def inside(points, target):
     # intersections within the bounds. The filter function also returns
     # an iterator which must be converted into a list before the remaining
     # elements can be counted. If the length is odd, then return true.
-    return (count + len(list(filter(
+    count += len(list(filter(
         # This lambda function is used by the filter to iterate through all
         # lines created by the points within the points list and to check if
         # the intersection with the horizontal line y is within bounds.
         # If it is, the point is kept within the array.
-        lambda i: (target[0]
-                   <= intersection(points[i], points[(i + 1) % len(points)], target[1])
-                   <= max_x),
+        lambda i: in_bounds(
+            target[0],
+            intersection(points[i], points[(i + 1) % len(points)], target[1]),
+            max_x),
         range(len(points))
-    ))) % 2) == 1
+    )))
+
+    return count % 2 == 1
 
 
-print(inside([(2, 3), (5, 7), (8, 5), (6, 2), (4, 1)], (2, 4)))
+polygon = [(1, 1), (2, 3), (3, 1)]
+point = (2, 2)
+print(in_polygon(polygon, point))
