b89dd7074cc83744d6112c33d190c8c7f0fae8e9
Allison <allisondr@msoe.edu>
2024-02-10 22:28:19 -0600

    Removed redundant checks and code

--- point-in-polygon.py
+++ point-in-polygon.py
@@ -1,20 +1,17 @@
 # The intersection function returns the point of intersection between the
 # line that is made from p1 and p0 and the horizontal line y.
 def intersection(p1, p0, y):
-    return (y * (p1[0] - p0[0]) - p0[1] * (p1[0] - p0[0]) - p0[0] * (p1[1] - p0[1])) / (p1[1] - p0[1]), y
-
-
-# The in_bounds function returns true if the point is in the bounds
-def in_bounds(bounds, point):
-    return bounds['min_y'] <= point[1] <= bounds['max_y'] and bounds['min_x'] <= point[0] <= bounds['max_x']
+    return (y * (p1[0] - p0[0]) - p0[1] * (p1[0] - p0[0]) - p0[0] * (p1[1] - p0[1])) / (p1[1] - p0[1])
 
 
 # The inside function determines if a point is inside a polygon made of other points.
 def inside(points, target):
+    count = 1 if points[0][1] == target[1] else 0
+    max_x = points[0][0]
     # Finds the bounds that the intersections must be made in.
-    min_y = min(points, key=lambda p: p[1])[1]
-    max_y = max(points, key=lambda p: p[1])[1]
-    max_x = max(points, key=lambda p: p[0])[0]
+    for p in points:
+        max_x = p[0] if max_x < p[0] else max_x
+        count = count + 1 if p[1] == target[1] else count
     # min_x is the x value of the target point.
 
     # A point is within the polygon if the number of intersections
@@ -25,20 +22,16 @@ def inside(points, target):
     # intersections within the bounds. The filter function also returns
     # an iterator which must be converted into a list before the remaining
     # elements can be counted. If the length is odd, then return true.
-    return len(list(filter(
+    return (count + len(list(filter(
         # This lambda function is used by the filter to iterate through all
         # lines created by the points within the points list and to check if
         # the intersection with the horizontal line y is within bounds.
         # If it is, the point is kept within the array.
-        lambda i: in_bounds(
-            {'min_y': min_y, 'min_x': target[0], 'max_y': max_y, 'max_x': max_x},
-            intersection(
-                points[i],
-                points[(i + 1) % len(points)],
-                target[1]
-            )
-        ),
+        lambda i: (target[0]
+                   <= intersection(points[i], points[(i + 1) % len(points)], target[1])
+                   <= max_x),
         range(len(points))
-    ))) % 2 == 1
+    ))) % 2) == 1
 
-print(inside([(2, 3), (5, 7), (8, 5), (6, 2), (4, 1)], (100, 100)))
+
+print(inside([(2, 3), (5, 7), (8, 5), (6, 2), (4, 1)], (2, 4)))
